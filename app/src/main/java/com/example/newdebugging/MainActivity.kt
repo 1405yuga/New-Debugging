0b77fcb14fd3ad0550de762c1dc9b6b46f559fa1
Yuga Ganesh Vasaikar <yugav2001@gmail.com>
2023-06-19 14:02:23 +0530

    creating division error function

--- app/src/main/java/com/example/newdebugging/MainActivity.kt
+++ app/src/main/java/com/example/newdebugging/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.newdebugging
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        division()
+    }
+    fun division() {
+        val numerator = 60
+        var denominator = 4
+        repeat(5) {
+            Log.v(TAG, "${numerator / denominator}")
+            denominator--
+        }
     }
 }
